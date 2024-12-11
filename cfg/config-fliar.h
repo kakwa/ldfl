381fd9b64c9ee33785366d1751140b83d37725b8
kakwa <carpentier.pf@gmail.com>
2024-12-11 23:27:11 +0100

    rollback accidental formatting

--- cfg/config-fliar.h
+++ cfg/config-fliar.h
@@ -1,13 +1,20 @@
 static const mapping_t mappings[] = {
     /* name                  search_pattern               type                   replace_pattern   target         */
-    {"temporary redirect", ".*/temporary/\\([^/]*\\)$", MAPPING_TYPE_REGEX, "/tmp/\\1", NULL},
-    {"memory open", ".*/file[0-9].txt", MAPPING_TYPE_MEM_OPEN, NULL, NULL},
-    {"static file", ".*/static.bin", MAPPING_TYPE_STATIC, NULL, default_blob},
-    {"default deny", ".*", MAPPING_TYPE_DENY, NULL, NULL}};
+    { "temporary redirect",  ".*/temporary/\\([^/]*\\)$", MAPPING_TYPE_REGEX,    "/tmp/\\1",       NULL        },
+    { "memory open",         ".*/file[0-9].txt",          MAPPING_TYPE_MEM_OPEN,  NULL,            NULL        },
+    { "static file",         ".*/static.bin",             MAPPING_TYPE_STATIC,    NULL,            default_blob},
+    {"default deny",         ".*",                        MAPPING_TYPE_DENY,      NULL,            NULL}
+};
 
-static const settings_t settings = {.log_level = "debug", .cache_first = true};
+static const settings_t settings = {
+    .log_level   = "debug",
+    .cache_first = true
+};
 
 static const configuration_t config = {
-    .settings = settings, .mappings = mappings, .mappings_count = sizeof(mappings) / sizeof(mappings[0])};
+    .settings       = settings,
+    .mappings       = mappings,
+    .mappings_count = sizeof(mappings) / sizeof(mappings[0])
+};
 
 /* ADDITIONAL BLOBS */
