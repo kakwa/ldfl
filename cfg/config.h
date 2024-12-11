88db443ad02b9e659b85225f102dd89b5eb7d3fe
kakwa <carpentier.pf@gmail.com>
2024-12-11 08:48:07 +0100

    adding example for hardcoded configuration

---
+++ cfg/config.h
@@ -0,0 +1,19 @@
+static const mapping_t mappings[] = {
+    /* name                  search_pattern              type                    replace_pattern   target         */
+    { "temporary redirect",  ".*/temporary/\\([^/]*\\)$", MAPPING_TYPE_REGEX,    "/tmp/\\1",       NULL        },
+    { "memory open",         ".*/file[0-9].txt",          MAPPING_TYPE_MEM_OPEN,  NULL,            NULL        },
+    { "static file",         ".*/static.bin",             MAPPING_TYPE_STATIC,    NULL,            default_blob}
+};
+
+static const settings_t settings = {
+    .log_level   = "debug",
+    .cache_first = true
+};
+
+static const configuration_t config = {
+    .settings       = settings,
+    .mappings       = mappings,
+    .mappings_count = sizeof(mappings) / sizeof(mappings[0])
+};
+
+/* ADDITIONAL BLOBS */
