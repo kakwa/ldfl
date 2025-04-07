9ea62ab465c576681b51c4e266309a0c219efe92
kakwa <carpentier.pf@gmail.com>
2025-04-07 14:01:16 +0200

    factorize valgrind tests

---
+++ misc/valgrind.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd  $(dirname $0)/..
+
+for ftest in fliar-test-*
+do
+    echo "checking '$ftest' for memleaks"
+    valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes --track-origins=yes --error-exitcode=42 ./$ftest || exit 1
+done
