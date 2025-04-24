e0bd4d8dd617c82546e1c65b374eddcd9d118af0
kakwa <carpentier.pf@gmail.com>
2025-04-25 00:39:18 +0200

    improve exec tests

--- tests/tests-exec.c
+++ tests/tests-exec.c
@@ -13,135 +13,147 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include "ldfl.c"
+#include <errno.h>
 
-#define TEST_OUTPUT_FILE "tests/exec_test_output.txt"
+#include "../lib/ldfl.c"
 
-#define CU_ASSERT_STRING_CONTAINS(haystack, needle)                                                                    \
-    do {                                                                                                               \
-        if (strstr(haystack, needle) == NULL) {                                                                        \
-            printf("```\n%s\n```\n not found in\n ```\n%s\n```'\n", needle, haystack);                                 \
-            CU_FAIL("CU_ASSERT_STRING_CONTAINS failed")                                                                \
-        } else {                                                                                                       \
-            CU_PASS("CU_ASSERT_STRING_CONTAINS passed");                                                               \
-        }                                                                                                              \
-    } while (0)
+#define EXEC_TEST_CONFIG "./tests/exec-config.json"
 
-// Helper function to read output from file
-static char *read_output_file(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file)
-        return NULL;
-
-    fseek(file, 0, SEEK_END);
-    long size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    char *content = malloc(size + 1);
-    fread(content, 1, size, file);
-    content[size] = '\0';
-
-    fclose(file);
-    return content;
+int mock_execve(const char *pathname, char *const argv[], char *const envp[]) {
+    printf("mock_execp: %s\n", pathname);
+    return 0;
 }
-
-typedef enum { EXECVE, EXECL, EXECLP, EXECV, EXECVP } exec_type_t;
-
-// Helper function to run a test command and capture output
-static int run_test_command(exec_type_t type, char **output) {
-    printf(">>> %d\n", type);
-    int fd = open(TEST_OUTPUT_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1)
-        return -1;
-
-    // Redirect stdout to our file
-    int saved_stdout = dup(STDOUT_FILENO);
-    dup2(fd, STDERR_FILENO);
-    close(fd);
-
-    char *argv[] = {"/bin/dir", NULL};
-    char *envp[] = {NULL};
-
-    switch (type) {
-    case EXECVE:
-        execve("/bin/dir", argv, envp);
-        break;
-    case EXECL:
-        execl("/bin/dir", "/bin/dir", NULL);
-        break;
-    case EXECLP:
-        execlp("/bin/dir", "/bin/dir", NULL);
-        break;
-    case EXECV:
-        execv("/bin/dir", argv);
-        break;
-    case EXECVP:
-        execvp("/bin/dir", argv);
-        break;
-    }
-
-    // Restore stdout
-    dup2(saved_stdout, STDERR_FILENO);
-    close(saved_stdout);
-
-    // If we get here, exec failed
-    *output = read_output_file(TEST_OUTPUT_FILE);
-    unlink(TEST_OUTPUT_FILE);
+int mock_execl(const char *pathname, const char *arg, ...) {
+    printf("mock_execl: %s\n", pathname);
+    return 0;
+}
+int mock_execlp(const char *file, const char *arg, ...) {
+    printf("mock_execlp: %s\n", file);
+    return 0;
+}
+int mock_execv(const char *pathname, char *const argv[]) {
+    printf("mock_execv: %s\n", pathname);
+    return 0;
+}
+int mock_execvp(const char *file, char *const argv[]) {
+    printf("mock_execvp: %s\n", file);
     return 0;
 }
 
-static void test_execve(void) {
-    char *output = NULL;
-
-    CU_ASSERT_EQUAL(run_test_command(EXECVE, &output), 0);
-    CU_ASSERT_PTR_NOT_NULL(output);
-    CU_ASSERT_STRING_CONTAINS(output, "noneexistantexecthere");
-    free(output);
+// Assign mocks to function pointers
+void init_mock_exec_functions() {
+    ldfl_init();
+    real_execve = mock_execve;
+    real_execl  = mock_execl;
+    real_execlp = mock_execlp;
+    real_execv  = mock_execv;
+    real_execvp = mock_execvp;
+    return;
 }
 
-static void test_execl(void) {
-    char *output = NULL;
+// Test execve function
+void test_execve(void) {
+    // Setup
+    init_mock_exec_functions();
+    CU_ASSERT_EQUAL(ldfl_parse_json_config(EXEC_TEST_CONFIG), 0);
 
-    CU_ASSERT_EQUAL(run_test_command(EXECL, &output), 0);
-    CU_ASSERT_PTR_NOT_NULL(output);
-    CU_ASSERT_STRING_CONTAINS(output, "noneexistantexecthere");
-    free(output);
+    char *argv[] = {"/bin/ls", NULL};
+    char *envp[] = {NULL};
+    execve("/bin/ls", argv, envp);
+
+    // Cleanup
+    ldfl_free_json_config();
 }
 
-static void test_execlp(void) {
-    char *output = NULL;
+void test_execve_deny(void) {
+    // Setup
+    init_mock_exec_functions();
+    CU_ASSERT_EQUAL(ldfl_parse_json_config(EXEC_TEST_CONFIG), 0);
 
-    CU_ASSERT_EQUAL(run_test_command(EXECLP, &output), 0);
-    CU_ASSERT_PTR_NOT_NULL(output);
-    CU_ASSERT_STRING_CONTAINS(output, "noneexistantexecthere");
-    free(output);
+    // Test deny
+    char *argv[] = {"/bin/echo", NULL};
+    char *envp[] = {NULL};
+    execve("/bin/echo", argv, envp);
+
+    // Cleanup
+    ldfl_free_json_config();
 }
 
-static void test_execv(void) {
-    char *output = NULL;
+void test_execve_redir(void) {
+    // Setup
+    init_mock_exec_functions();
+    CU_ASSERT_EQUAL(ldfl_parse_json_config(EXEC_TEST_CONFIG), 0);
 
-    CU_ASSERT_EQUAL(run_test_command(EXECV, &output), 0);
-    CU_ASSERT_PTR_NOT_NULL(output);
-    CU_ASSERT_STRING_CONTAINS(output, "noneexistantexecthere");
-    free(output);
+    char *argv[] = {"/bin/true", NULL};
+    char *envp[] = {NULL};
+    execve("/bin/true", argv, envp);
+
+    // Cleanup
+    ldfl_free_json_config();
 }
 
-static void test_execvp(void) {
-    char *output = NULL;
+// Test execl function
+void test_execl(void) {
+    // Setup
+    init_mock_exec_functions();
+    CU_ASSERT_EQUAL(ldfl_parse_json_config(EXEC_TEST_CONFIG), 0);
 
-    CU_ASSERT_EQUAL(run_test_command(EXECVP, &output), 0);
-    CU_ASSERT_PTR_NOT_NULL(output);
-    CU_ASSERT_STRING_CONTAINS(output, "noneexistantexecthere");
-    free(output);
+    // Test redirect
+    execl("/bin/ls", "/bin/ls", NULL);
+
+    // Cleanup
+    ldfl_free_json_config();
+}
+
+// Test execlp function
+void test_execlp(void) {
+    // Setup
+    init_mock_exec_functions();
+    CU_ASSERT_EQUAL(ldfl_parse_json_config(EXEC_TEST_CONFIG), 0);
+
+    // Test redirect
+    execlp("ls", "ls", NULL);
+
+    // Cleanup
+    ldfl_free_json_config();
+}
+
+// Test execv function
+void test_execv(void) {
+    // Setup
+    init_mock_exec_functions();
+    CU_ASSERT_EQUAL(ldfl_parse_json_config(EXEC_TEST_CONFIG), 0);
+
+    // Test redirect
+    char *argv[] = {"/bin/ls", NULL};
+    execv("/bin/ls", argv);
+
+    // Cleanup
+    ldfl_free_json_config();
+}
+
+// Test execvp function
+void test_execvp(void) {
+    // Setup
+    init_mock_exec_functions();
+
+    // Test redirect
+    char *argv[] = {"ls", NULL};
+    execvp("ls", argv);
+
+    // Cleanup
+    ldfl_free_json_config();
 }
 
 int main(void) {
-    setenv("LDFL_CONFIG", "./tests/exec-config.json", 1);
+    setenv("LDFL_CONFIG", EXEC_TEST_CONFIG, 1);
     CU_initialize_registry();
 
     // Add the new test suite
     CU_pSuite pSuite = CU_add_suite("Exec Tests", NULL, NULL);
     CU_add_test(pSuite, "test of execve()", test_execve);
+    CU_add_test(pSuite, "test of execve() (deny)", test_execve_deny);
+    CU_add_test(pSuite, "test of execve() (redir)", test_execve_redir);
     CU_add_test(pSuite, "test of execl()", test_execl);
     CU_add_test(pSuite, "test of execlp()", test_execlp);
     CU_add_test(pSuite, "test of execv()", test_execv);
